1070059c1e4174180ce877386293c33fc0323e7f
Alred <prottoyrudra7777@gmail.com>
2024-03-05 21:26:02 +0000

    fetched some stuff

---
+++ src/components/totalPostByPlatform.tsx
@@ -0,0 +1,110 @@
+"use client";
+import { useEffect, useState } from "react";
+import {
+  collection,
+  doc,
+  getDoc,
+  onSnapshot,
+  getFirestore,
+} from "firebase/firestore";
+import { Heading, Spinner, Box } from "@chakra-ui/react";
+import firebase_app from "../firebase/config";
+import { Pie } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const db = getFirestore(firebase_app);
+
+const TotalPostByPlatform = () => {
+  const [totalFacebookPost, setTotalFacebookPost] = useState(0);
+  const [totalInstagramPost, setTotalInstagramPost] = useState(0);
+  const [totalTwitterPost, setTotalTwitterPost] = useState(0);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTotalPost = async () => {
+      const facebookDocRef = doc(
+        collection(db, "analytics"),
+        "facebook-analytics"
+      );
+      const instagramDocRef = doc(
+        collection(db, "analytics"),
+        "insta-analytics"
+      );
+      const twitterDocRef = doc(
+        collection(db, "analytics"),
+        "twitter-analytics"
+      );
+
+      const [facebookDocSnap, instagramDocSnap, twitterDocSnap] =
+        await Promise.all([
+          getDoc(facebookDocRef),
+          getDoc(instagramDocRef),
+          getDoc(twitterDocRef),
+        ]);
+
+      if (facebookDocSnap.exists()) {
+        setTotalFacebookPost(facebookDocSnap.data().totalPost);
+      }
+      if (instagramDocSnap.exists()) {
+        setTotalInstagramPost(instagramDocSnap.data().totalPost);
+      }
+      if (twitterDocSnap.exists()) {
+        setTotalTwitterPost(twitterDocSnap.data().totalPost);
+      }
+
+      setLoading(false);
+    };
+
+    const unsubscribe = onSnapshot(
+      doc(collection(db, "posts"), "facebook"),
+      () => {
+        fetchTotalPost();
+      }
+    );
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <Box>
+      {loading ? (
+        <Box pos="fixed" left="45vw" top="45vh">
+          <Spinner />
+        </Box>
+      ) : (
+        <>
+          <Pie
+            data={{
+              labels: ["Facebook", "Instagram", "Twitter"],
+              datasets: [
+                {
+                  data: [
+                    totalFacebookPost,
+                    totalInstagramPost,
+                    totalTwitterPost,
+                  ],
+                  backgroundColor: ["#3b5998", "#C13584", "#1DA1F2"],
+                },
+              ],
+            }}
+          />
+
+          <Heading variant="h6">
+            Total Facebook Posts: {totalFacebookPost}
+          </Heading>
+          <Heading variant="h6">
+            Total Instagram Posts: {totalInstagramPost}
+          </Heading>
+          <Heading variant="h6">
+            Total Twitter Posts: {totalTwitterPost}
+          </Heading>
+        </>
+      )}
+    </Box>
+  );
+};
+
+export default TotalPostByPlatform;
